efabb43e445faefbe9fa0242f10cbdd24ebaffd4
Sam <sam.terrazas@yahoo.com>
2019-02-24 19:05:51 -0500

    updated

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,5 @@
 var friends = require("../data/friends.js");
-// var express = require('express');
 var path = require('path');
-// var router = express.Router()
 
 // API ROUTES
 module.exports = function (app) {
@@ -36,46 +34,8 @@ module.exports = function (app) {
           matchImage = friends[i].photo;
         }
       }
-      friends.push(userinput);
+      friends.push(userInput);
       res.join({ status: 'OK', matchName: matchName, matchImage: matchImage });
     };
   });
 };
-// /Users/samterrazas/Desktop/ga-tech/FriendFinder/app/data/friends.js
-// ROUTING
-// module.exports = function (app) {
-//   app.get("/api/friends", function (req, res) {
-//     res.json(friendsData);
-//   });
-
-//   app.post("/api/friends", function (req, res) {
-//     var bestMatch = {
-//       name: "",
-//       image: "",
-//       difference: 100
-//     }
-
-//     var userData = req.body;
-//     var userScores = userData.scores;
-
-//     var totalDifference = 0;
-
-//     for (var i = 0; i < friendsData.length; i++) {
-//       console.log(friendsData[i].name);
-//       totalDifference = 0;
-
-//       for (var j = 0; j < friendsData[i].scores.length; j++) {
-//         totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendsData[i].scores[j]));
-
-//         if (totalDifference <= bestMatch.difference) {
-//           bestMatch.name = friendsData[i].name;
-//           bestMatch.image = friendsData[i].image;
-//           bestMatch.difference = totalDifference;
-//         }
-//       }
-//     }
-//     friendsData.push(userData);
-
-//     res.json(bestMatch);
-//   });
-// };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,5 @@
 // DEPENDENCIES
 var path = require("path");
-// var express = require("express");
-// var router = express.Router();
 
 // ROUTES
 module.exports = function (app) {
@@ -13,15 +11,12 @@ module.exports = function (app) {
 
   // ROUTE TO SURVEY 
   app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "/survey.html"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   // IF NO ROUTE IS LOCATED IT WILL DEFAULT BACK TO HOME PAGE
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-  // module.exports = router;
+  // app.get("*", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/home.html"));
+  // });
 }
 
-
-// FriendFinder / app / public / survey.html
